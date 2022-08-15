b6abbe0b65dfcebc2afd0f9175ef70fc31cab98f
Lucas <lucasbivarfonseca@gmail.com>
2022-08-15 18:56:20 -0300

    adding env

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://task-back-diel.herokuapp.com/api',
+  baseURL: import.meta.env.VITE_BACKEND_URL,
 });
 
 export const getTasks = async () => {
